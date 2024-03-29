161cc84bf4cac61e5ceeaef3118dcfc8838172e2
carsontham <carsontham@outlook.com>
2024-03-30 01:24:26 +0800

    completed non-overlap intervals 30mar24, 1:24AM

---
+++ 32. Non-overlapping intervals [MEDIUM]/non_overlap_intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        count = 0
+
+        intervals.sort()
+        prevEnd = intervals[0][1]
+
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                count += 1
+                prevEnd = min(prevEnd, end)
+
+        return count 
+        
